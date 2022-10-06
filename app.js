1a06152ff8e42c8aa2592d19e4f57d1944599e55
Abhinavjain123 <abhinavsra@gmail.com>
2022-10-06 09:47:12 +0530

    update app.js

--- app.js
+++ app.js
@@ -26,14 +26,74 @@ app.get("/wardenHome",function(req,res){
     res.sendFile(__dirname + "/wardenHome.html")
 });
 
+// DATABASE connection
+const {Client} = require("pg");
+const client = new Client({
+    host:"localhost",
+    user:"postgres",
+    port:5432,
+    password:"Abhisql",
+    database:"Hosteldb"
+})
+client.connect();
+
 app.post('/',function(req,res){
     var p = req.body.whoami;
-    if(p=='std')
-	    res.redirect('/studentHome');
-    else if(p=='adm')
-	    res.redirect('/adminHome');
-    else
-        res.redirect('/wardenHome');
+    var p_id = req.body.stdid;
+    var p_passwd = req.body.passwd;
+    
+    if(p=='std'){
+        client.query(`select student_passwd from student where student_id = '${p_id}'`, (err, res2) => {
+            console.log(res2);
+            if(err) console.log(err.message);
+            else if (res2.rowCount == 0){
+                console.log("Student not registered");
+                res.send("Student not registered");
+            }
+            else if(p_passwd==res2.rows[0].student_passwd){
+                res.redirect('/studentHome');
+            }
+            else{
+                console.log("Password Not Matched.");
+                res.send("Password Not Matched.");
+            }
+        })
+        
+    }
+    else if(p=='adm'){
+        client.query(`select admin_passwd from admin where admin_id = '${p_id}'`, (err, res2) => {
+            console.log(res2);
+            if (err) console.log(err.message);
+            else if (res2.rowCount == 0) {
+                console.log("Admin not found");
+                res.send("Admin not found");
+            }
+            else if (p_passwd == res2.rows[0].admin_passwd) {
+                res.redirect('/adminHome');
+            }
+            else {
+                console.log("Password Not Matched.");
+                res.send("Password Not Matched.");
+            }
+        })
+    }
+    else{
+        client.query(`select warden_passwd from warden where warden_id = '${p_id}'`, (err, res2) => {
+            console.log(res2);
+            if (err) console.log(err.message);
+            else if (res2.rowCount == 0) {
+                console.log("warden not registered");
+                res.send("warden not registered");
+            }
+            else if (p_passwd == res2.rows[0].student_passwd) {
+                res.redirect('/wardenHome');
+            }
+            else {
+                console.log("Password Not Matched.");
+                res.send("Password Not Matched.");
+            }
+        })
+    }
 })
 
 
